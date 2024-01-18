02e7a8d9498d5d4447109481c0ebab91f308f74c
ccs-isaac <157028638+ccs-isaac@users.noreply.github.com>
2024-01-18 08:46:52 -0500

    Update interface.py

--- selfdrive/car/ford/interface.py
+++ selfdrive/car/ford/interface.py
@@ -15,7 +15,7 @@ class CarInterface(CarInterfaceBase):
   def _get_params(ret, candidate, fingerprint, car_fw, experimental_long, docs):
     ret.carName = "ford"
 
-    ret.radarUnavailable = True
+    ret.radarUnavailable = candidate not in CANFD_CAR
     ret.steerControlType = car.CarParams.SteerControlType.angle
     ret.steerActuatorDelay = 0.2
     ret.steerLimitTimer = 1.0
